f53dfd034c7ea35be97481fc8602dc5fae9d87a7
Vikranth12 <56485625+Vikranth12@users.noreply.github.com>
2020-08-11 20:35:58 +0530

    Create insertion_sort.c

---
+++ insertion_sort.c
@@ -0,0 +1,35 @@
+    #include<stdio.h>
+    #include<stdlib.h>
+    #include<string.h>
+    #include<math.h>
+    
+    void bubble(int *arr,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && temp<arr[j])
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
+    int main()
+    {
+        int *ar;
+        int n;
+        printf("enter the size of the array :");
+        scanf("%d",&n);
+        ar=(int *)malloc(n*sizeof(int));
+        for(int i=0;i<n;i++)
+            scanf("%d",&ar[i]);
+        bubble(ar,n);
+        for(int j=0;j<n;j++)
+            printf("%d  ",ar[j]);
+        printf("\n");
+        return 0;
+    }
+
